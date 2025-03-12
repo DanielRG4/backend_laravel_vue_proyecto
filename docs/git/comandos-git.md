c5bbbfcf16f7b27621619b3ce1b7d3143b8e99e2
DanielRG4 <108098485+DanielRG4@users.noreply.github.com>
2025-03-12 16:49:37 -0600

    Proyecto Base (CRUD Usuarios)

---
+++ docs/git/comandos-git.md
@@ -0,0 +1,27 @@
+# Comandos de Git (Iniciales)
+## Requerimientos
+- Descargar GIT
+- Crear una cuenta en GITHUB
+## Configurar GIT
+- ejecutar los siguientes comandos
+```
+git config --global "Su nombre"
+git config --global "correo@mail.com"
+```
+## Comando Iniciales
+## Crear un Respositorio en GITHUB
+## Inicializar un Repositorio Local
+```
+git init
+```
+### Referencia del Repositorio (Local) al Repositorio (REMOTO)
+
+```
+
+git remote add origin https://github.com/DanielRG4/backend_laravel_vue_proyecto.git
+```
+## ACTUALIZAR EL REPOSITORIO
+
+```
+git add .
+```
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\RoleContoller;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+//CRUD API REST USER
+Route:: get("/user",[UserController::class,"funListar"]);
+Route:: post("/user",[UserController::class,"funGuardar"]);
+Route:: get("/user/{id}",[UserController::class, "funMostrar"]);
+Route:: put("/user/{id}",[UserController::class, "funModificar"]);
+Route:: delete("/user/{id}",[UserController::class, "funEliminar"]);
+
+//Crud Roles
+
+Route::apiResource("roles", RoleContoller::class);
